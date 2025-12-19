3c9ff7afeade326235ffda79673b3d7470ae1c8d
haris <malikmohdharis89gmail.com>
2025-12-20 00:18:06 +0500

    First Assignment Using React

---
+++ myProject/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from "react"
+import {CounterButtons} from "./components/counter.jsx"
+import "./style/style.css"
+function App() {
+  
+  return (
+    <>
+  <CounterButtons />
+    </>
+  )
+}
+
+export default App
